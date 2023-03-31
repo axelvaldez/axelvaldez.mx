d0c8d22911c6cc0d329ebad9089f1cc227dead26
Axel Valdez <axel@paskola.com>
2023-03-31 09:24:52 -0700

    persistent dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,22 @@
 darkMode = document.getElementById("toggle");
 rootNode = document.querySelector(":root");
 
+manualDarkMode = localStorage.getItem("theme");
+
 function dark(enable){
     if (enable){
         rootNode.classList.add('dark');
         darkMode.checked = true;
+        localStorage.setItem("theme", "dark");
     }else{
         rootNode.classList.remove('dark');
         darkMode.checked = false;
+        localStorage.setItem("theme", "light");
     }
 }
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+if (
+    window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && manualDarkMode != "light") {
     dark(true);
 }
 else{
